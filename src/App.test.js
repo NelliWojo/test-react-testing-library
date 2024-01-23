cb948bda1fc9b8f9df78e4a6e77ea83e18b3d0b8
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-01-23 12:11:27 +0100

    init :tada: setup & first tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test App", () => {
+  test("test 1", () => {
+    render(<App />);
+    const hiThereElement = screen.getByText(/hi there/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input value/i);
+
+    expect(hiThereElement).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    // expect(input).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+
+  test("test 2", () => {
+    render(<App />);
+    const hiThereElement = screen.queryByText(/hi there 2/i);
+    expect(hiThereElement).toBeNull();
+  });
+
+  // test("test 3", () => {
+  //   render(<App />);
+  //   const hiThereElement = screen.findByText(/hi there 2/i);
+  //   expect(hiThereElement).toBeNull();
+  // });
+
+  // screen.debug();
 });

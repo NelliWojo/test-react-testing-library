48908cb4d4e85375a50523d3193309e984a52021
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-01-23 14:18:28 +0100

    test :sparkles: user events (click, input)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState("");
+
+  const onClick = () => setToggle((prev) => !prev);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+
   return (
     <div>
+      <h1 data-testid="value-el">{value}</h1>
+      {toggle === true && <div data-testid="toggle-el">Toggle</div>}
+      {data && <div style={{ color: "red" }}>Data</div>}
       <h1>Hi there</h1>
-      <button>Click me</button>
-      <input type="text" placeholder="input value" />
+      <button data-testid="toggle-btn" onClick={onClick}>
+        click me
+      </button>
+      <input
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        placeholder="input value"
+      />
     </div>
   );
 }
